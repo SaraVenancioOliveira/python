b169f28988f9aec2a4f6722afed7c6016f2dcbfb
SaraVenancioOliveira <149601534+SaraVenancioOliveira@users.noreply.github.com>
2025-02-13 18:11:51 +0000

    btyugu

--- primeiro.py
+++ primeiro.py
@@ -1,3 +1,27 @@
-print("Olá mundo")
-nome = input ("Qual é o seu nome")
-print("Bem vinda", nome)
+print(***********************************)
+print("Bem vindos ao jogo da adivinhação")
+print(***********************************)
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+while (rodada <= total_de tentativas):
+    print("Você digitou o número", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("parabens, você acertou")
+        else
+        if(maior)
+        print("o seu chute foi maior do que o número secreto")
+        elf(menor):
+        print("o seu chute foi menor do queo número secreto")
+        rodada = rodada +1
+        print("fim de jogo")
+
+
