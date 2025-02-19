21e2acdbd6712d1086a905b75fb4d62829c8fbaa
SaraVenancioOliveira <149601534+SaraVenancioOliveira@users.noreply.github.com>
2025-02-19 17:04:28 +0000

    cadastro

---
+++ cadastro.py
@@ -0,0 +1,7 @@
+nome = str(input("digite seu nome"))
+cpf = float(input("digite seu cpf"))
+rg = float(input("digite o seu rg"))
+numero = int(input("digite o seu numero de celular"))
+idade = int(input("digite sua idade"))
+
+print(f"voce confirma seu cadastro? \n nome: {nome} \n cpf: {cpf} \n rg: {rg} \n numero {numero} \n idade {idade}")
--- primeiro.py
+++ primeiro.py
@@ -1,16 +1,24 @@
 import random
 
-print(***********************************)
+print(*"**********************************")
 print("Bem vindos ao jogo da adivinhação")
-print(***********************************)
+print("***********************************")
 
 numero_secreto = random.randrange(1,101)
 total_de_tentativas = 10
 rodada = 1
 
-while (rodada <= total_de tentativas):
+while (rodada <= total_de_tentativas):
+    print(f"Tentativa {rodada} de {total_de_tentativas}")
+
+    chute_str = input("digite o seu número:")
     print("Você digitou o número", chute_str)
-    chute = int(chute_str)
+    
+    try:
+        chute = int(chute_str)
+    except ValueError:
+        print("caractere invalido. Use apenas números")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -24,6 +32,6 @@ while (rodada <= total_de tentativas):
         elif(menor):
            print("o seu chute foi menor do queo número secreto")
     rodada = rodada +1
-print("fim de jogo")
+print("fim de jogo", numero_secreto)
 
 
