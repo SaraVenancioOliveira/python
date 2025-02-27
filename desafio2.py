06a8a830208d902d40a2b83e84e194720767aa44
SaraVenancioOliveira <149601534+SaraVenancioOliveira@users.noreply.github.com>
2025-02-27 17:22:02 +0000

    123

---
+++ desafio2.py
@@ -0,0 +1,6 @@
+nome = str(input("qual o seu nome?"))
+dataVencimento = str(input("qual é a data de vencimento?"))
+Valor = str(input("qual é o valor?"))
+
+print(f"Olá {nome} a data de vencimento é {dataVencimento} o valor é {Valor}" )
+
